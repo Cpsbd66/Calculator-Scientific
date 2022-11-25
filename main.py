9a31ade4abcf54678896f6422b14e2c16c7e974e
Tapantor Das <109220196+cpsbd66@users.noreply.github.com>
2022-11-25 17:55:57 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,111 +1,201 @@
 from tkinter import *
+import math
+from pygame import mixer
+import speech_recognition
 
-first_number=second_number=operator=None
+mixer.init()
 
-def get_digit(digit):
-    current = result_label['text']
-    new = current + str(digit)
-    result_label.config(text=new)
 
-def clear():
-    result_label.config(text='')
+def click(value):
+    ex = entryField.get()  # 789 ex[0:len(ex)-1]
+    answer = ''
 
-def get_operator(op):
-    global first_number,operator
+    try:
 
-    first_number = int(result_label['text'])
-    operator = op
-    result_label.config(text='')
+        if value == 'C':
+            ex = ex[0:len(ex) - 1]  # 78
+            entryField.delete(0, END)
+            entryField.insert(0, ex)
+            return
 
-def get_result():
-    global first_number,second_number,operator
+        elif value == 'CE':
+            entryField.delete(0, END)
 
-    second_number = int(result_label['text'])
+        elif value == '√':
+            answer = math.sqrt(eval(ex))
 
-    if operator == '+':
-        result_label.config(text=str(first_number+second_number))
-    elif operator == '-':
-        result_label.config(text=str(first_number - second_number))
-    elif operator == '*':
-        result_label.config(text=str(first_number * second_number))
-    else:
-        if second_number == 0:
-            result_label.config(text='Error')
+        elif value == 'π':
+            answer = math.pi
+
+        elif value == 'cosθ':
+            answer = math.cos(math.radians(eval(ex)))
+
+        elif value == 'tanθ':
+            answer = math.tan(math.radians(eval(ex)))
+
+        elif value == 'sinθ':
+            answer = math.sin(math.radians(eval(ex)))
+
+        elif value == '2π':
+            answer = 2 * math.pi
+
+        elif value == 'cosh':
+            answer = math.cosh(eval(ex))
+
+        elif value == 'tanh':
+            answer = math.tanh(eval(ex))
+
+        elif value == 'sinh':
+            answer = math.sinh(eval(ex))
+
+        elif value == chr(8731):
+            answer = eval(ex) ** (1 / 3)
+
+        elif value == 'x\u02b8':  # 7**2
+            entryField.insert(END, '**')
+            return
+
+        elif value == 'x\u00B3':
+            answer = eval(ex) ** 3
+
+        elif value == 'x\u00B2':
+            answer = eval(ex) ** 2
+
+        elif value == 'ln':
+            answer = math.log2(eval(ex))
+
+        elif value == 'deg':
+            answer = math.degrees(eval(ex))
+
+        elif value == "rad":
+            answer = math.radians(eval(ex))
+
+        elif value == 'e':
+            answer = math.e
+
+        elif value == 'log₁₀':
+            answer = math.log10(eval(ex))
+
+        elif value == 'x!':
+            answer = math.factorial(ex)
+
+        elif value == chr(247):  # 7/2=3.5
+            entryField.insert(END, "/")
+            return
+
+        elif value == '=':
+            answer = eval(ex)
+
         else:
-            result_label.config(text=str(round(first_number / second_number,2)))
+            entryField.insert(END, value)
+            return
+
+        entryField.delete(0, END)
+        entryField.insert(0, answer)
+
+    except SyntaxError:
+        pass
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+def lcm(a,b):
+    l=math.lcm(a,b)
+    return l
+
+def hcf(a,b):
+    h=math.gcd(a,b)
+    return h
+
+operations={'ADD':add,'ADDITION':add,'SUM':add,'PLUS':add,
+            'SUBTRACTION':sub , 'DIFFERENCE':sub , 'MINUS':sub , 'SUBTRACT':sub,
+            'PRODUCT': mul, 'MULTIPLICATION': mul,'MULTIPLY': mul,
+            'DIVISION': div, 'DIV': div, 'DIVIDE': div,
+            'LCM':lcm , 'HCF':hcf,
+            'MOD':mod ,'REMAINDER':mod , 'MODULUS':mod }
+
+
+def findNumbers(t):
+    l=[]
+    for num in t:
+        try:
+            l.append(int(num))
+        except ValueError:
+            pass
+    return l
+
+def audio():
+    mixer.music.load('music1.mp3')
+    mixer.music.play()
+    sr = speech_recognition.Recognizer()
+    with speech_recognition.Microphone()as m:
+        try:
+            sr.adjust_for_ambient_noise(m,duration=0.2)
+            voice=sr.listen(m)
+            text=sr.recognize_google(voice)
+
+            mixer.music.load('music2.mp3')
+            mixer.music.play()
+            text_list=text.split(' ')
+            for word in text_list:
+                if word.upper() in operations.keys():
+                    l=findNumbers(text_list)
+                    print(l)
+                    result=operations[word.upper()](l[0],l[1]) #mul(5.0,6.0)
+                    entryField.delete(0,END)
+                    entryField.insert(END,result)
+
+                else:
+                    pass
+
+
+        except:
+            pass
+
+
 
 root = Tk()
-root.title('Calculator')
-root.geometry('280x380')
-root.resizable(0,0)
-root.configure(background='black')
+root.title('Smart Calculator')
+root.config(bg='dodgerblue3')
+root.geometry('680x486+100+100')
 
-result_label = Label(root,text='',bg='black',fg='white')
-result_label.grid(row=0,column=0,columnspan=5,pady=(50,25),sticky='w')
-result_label.config(font=('verdana',30,'bold'))
+logoImage = PhotoImage(file='logo.png')
+logoLabel = Label(root, image=logoImage, bg='dodgerblue3')
+logoLabel.grid(row=0, column=0)
 
-btn7 = Button(root,text='7',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(7))
-btn7.grid(row=1,column=0)
-btn7.config(font=('verdana',14))
+entryField = Entry(root, font=('arial', 20, 'bold'), bg='dodgerblue3', fg='white', bd=10, relief=SUNKEN, width=30)
+entryField.grid(row=0, column=0, columnspan=8)
 
-btn8 = Button(root,text='8',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(8))
-btn8.grid(row=1,column=1)
-btn8.config(font=('verdana',14))
+micImage = PhotoImage(file='microphone.png')
+micButton = Button(root, image=micImage, bd=0, bg='dodgerblue3', activebackground='dodgerblue3'
+                   ,command=audio)
+micButton.grid(row=0, column=7)
 
-btn9 = Button(root,text='9',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(9))
-btn9.grid(row=1,column=2)
-btn9.config(font=('verdana',14))
+button_text_list = ["C", "CE", "√", "+", "π", "cosθ", "tanθ", "sinθ",
+                    "1", "2", "3", "-", "2π", "cosh", "tanh", "sinh",
+                    "4", "5", "6", "*", chr(8731), "x\u02b8", "x\u00B3", "x\u00B2",
+                    "7", "8", "9", chr(247), "ln", "deg", "rad", "e",
+                    "0", ".", "%", "=", "log₁₀", "(", ")", "x!"]
+rowvalue = 1
+columnvalue = 0
+for i in button_text_list:
 
-btn_add = Button(root,text='+',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_operator('+'))
-btn_add.grid(row=1,column=3)
-btn_add.config(font=('verdana',14))
-
-btn4 = Button(root,text='4',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(4))
-btn4.grid(row=2,column=0)
-btn4.config(font=('verdana',14))
-
-btn5 = Button(root,text='5',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(5))
-btn5.grid(row=2,column=1)
-btn5.config(font=('verdana',14))
-
-btn6 = Button(root,text='6',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(6))
-btn6.grid(row=2,column=2)
-btn6.config(font=('verdana',14))
-
-btn_sub = Button(root,text='-',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_operator('-'))
-btn_sub.grid(row=2,column=3)
-btn_sub.config(font=('verdana',14))
-
-btn1 = Button(root,text='1',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(1))
-btn1.grid(row=3,column=0)
-btn1.config(font=('verdana',14))
-
-btn2 = Button(root,text='2',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(2))
-btn2.grid(row=3,column=1)
-btn2.config(font=('verdana',14))
-
-btn3 = Button(root,text='3',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(3))
-btn3.grid(row=3,column=2)
-btn3.config(font=('verdana',14))
-
-btn_mul = Button(root,text='*',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_operator('*'))
-btn_mul.grid(row=3,column=3)
-btn_mul.config(font=('verdana',14))
-
-btn_clr = Button(root,text='C',bg='#00a65a',fg='white',width=5,height=2,command=lambda :clear())
-btn_clr.grid(row=4,column=0)
-btn_clr.config(font=('verdana',14))
-
-btn0 = Button(root,text='0',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(0))
-btn0.grid(row=4,column=1)
-btn0.config(font=('verdana',14))
-
-btn_equals = Button(root,text='=',bg='#00a65a',fg='white',width=5,height=2,command=get_result)
-btn_equals.grid(row=4,column=2)
-btn_equals.config(font=('verdana',14))
-
-btn_div = Button(root,text='/',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_operator('/'))
-btn_div.grid(row=4,column=3)
-btn_div.config(font=('verdana',14))
+    button = Button(root, width=5, height=2, bd=2, relief=SUNKEN, text=i, bg='dodgerblue3', fg='white',
+                    font=('arial', 18, 'bold'), activebackground='dodgerblue3', command=lambda button=i: click(button))
+    button.grid(row=rowvalue, column=columnvalue, pady=1)
+    columnvalue += 1
+    if columnvalue > 7:
+        rowvalue += 1
+        columnvalue = 0
 
 root.mainloop()
